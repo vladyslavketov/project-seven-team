4b7c865f4e428fb0a0cb2228f738e1bae7b8f466
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-08-09 18:51:10 +0300

    Update code
    update:
    - icons.svg
    
    add:
    - html - scss - js - different modal
    - scss - common - different padding for breakpoint
    
    fixbag:
    - html - contacts - (add ")
    - css - footer - (del margin, add padding) + (add &#169 - (c))

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,77 @@
+// шаблон
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// кнопка Buy-now
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-buy-open]"),
+    closeModalBtn: document.querySelector("[data-modal-buy-close]"),
+    modal: document.querySelector("[data-modal-buy]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// кнопка Read more
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-read-open]"),
+    closeModalBtn: document.querySelector("[data-modal-read-close]"),
+    modal: document.querySelector("[data-modal-read]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// кнопка Our locations
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-locations-open]"),
+    closeModalBtn: document.querySelector("[data-modal-locations-close]"),
+    modal: document.querySelector("[data-modal-locations]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// кнопка franchise
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-franchise-open]"),
+    closeModalBtn: document.querySelector("[data-modal-franchise-close]"),
+    modal: document.querySelector("[data-modal-franchise]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
