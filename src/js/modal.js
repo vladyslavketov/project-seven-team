77bf93c096c85b0baa271f79dd68a582c8a5f0e7
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-08-11 12:48:57 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -19,67 +19,67 @@
 // кнопка Buy-now
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open--buy]'),
-    closeModalBtn: document.querySelector('[data-modal-close--buy]'),
-    modal: document.querySelector('[data-modal--buy]'),
+    openModalBtn: document.querySelector("[data-modal-open--buy]"),
+    closeModalBtn: document.querySelector("[data-modal-close--buy]"),
+    modal: document.querySelector("[data-modal--buy]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
 // кнопка Read more
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open--read]'),
-    closeModalBtn: document.querySelector('[data-modal-close--read]'),
-    modal: document.querySelector('[data-modal--read]'),
+    openModalBtn: document.querySelector("[data-modal-open--read]"),
+    closeModalBtn: document.querySelector("[data-modal-close--read]"),
+    modal: document.querySelector("[data-modal--read]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
 // кнопка Our locations
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open--locations]'),
-    closeModalBtn: document.querySelector('[data-modal-close--locations]'),
-    modal: document.querySelector('[data-modal--locations]'),
+    openModalBtn: document.querySelector("[data-modal-open--locations]"),
+    closeModalBtn: document.querySelector("[data-modal-close--locations]"),
+    modal: document.querySelector("[data-modal--locations]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
 // кнопка franchise
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open--franchise]'),
-    closeModalBtn: document.querySelector('[data-modal-close--franchise]'),
-    modal: document.querySelector('[data-modal--franchise]'),
+    openModalBtn: document.querySelector("[data-modal-open--franchise]"),
+    closeModalBtn: document.querySelector("[data-modal-close--franchise]"),
+    modal: document.querySelector("[data-modal--franchise]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
