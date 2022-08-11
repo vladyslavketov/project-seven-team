600080c1951379887f169bb2ce504e9a641e928a
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-08-11 19:39:33 +0300

    make js

--- src/js/modal.js
+++ src/js/modal.js
@@ -38,11 +38,13 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open--read]"),
     closeModalBtn: document.querySelector("[data-modal-close--read]"),
+    closeModalBtnAdd: document.querySelector("[data-modal-read--close]"),
     modal: document.querySelector("[data-modal--read]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtnAdd.addEventListener("click", toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open")
